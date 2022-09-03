e3f6f49853a13805d47420a5fb64d24e7d311c53
Chinmay Naik <94610060+ChinmayNaik27@users.noreply.github.com>
2022-09-03 08:37:50 +0530

    spark-programs

--- Sparkcore/pyspark90thproc.py
+++ Sparkcore/pyspark90thproc.py
@@ -4,33 +4,34 @@ from pyspark.sql.functions import *
 from pyspark.sql.types import *
 spark = SparkSession.builder.master("local[*]").appName("test").getOrCreate()
 sc = spark.sparkContext
-def read_json_file(df):
+def read_xml_nested_file(df):
     cols=[]
-    for col_name in df.schema.name:
-        if isinstance(df.schema[col_name].dataType(),ArrayType):
+    for col_name in df.schema.names:
+        if isinstance(df.schema[col_name].dataType,ArrayType):
             df=df.withColumn(col_name,explode(col_name))
             cols.append(col_name)
-        elif isinstance(df.shema[col_name].dataType(),StructType):
+        elif isinstance(df.schema[col_name].dataType,StructType):
             for field in df.schema[col_name].dataType.fields:
                 print(field)
-                cols.append(col(col_name)+"."+field.name.alias(col_name+"_"+field.name))
+                cols.append(col(col_name+"."+field.name).alias(col_name+"_"+field.name))
         else:
             cols.append(col_name)
     df.select(cols)
     return df
 def flatten(df):
-    read_json_flag=True
-    while read_json_flag:
-        df=read_json_file(df)
-        read_json_flag=False
-        for col_name in df.schema.name:
-            if isinstance(df[col_name].dataType(),ArrayType):
-                read_json_flag=True
-            elif isinstance(df[col_name].dataType(),StructType):
-                read_json_flag=True
+    read_nested_xml_flag=True
+    while read_nested_xml_flag:
+        df=read_xml_nested_file(df)
+        read_nested_xml_flag=False
+        for col_name in df.schema.names:
+            if isinstance(df[col_name].dataType,ArrayType):
+                read_nested_xml_flag=True
+            elif isinstance(df[col_name].dataType,StructType):
+                read_nested_xml_flag=True
+    return df
 #creating df
-data="C:\\bigdata\\DATASETS\\books.xml"
-odata="E:\\outputofxml\complexxml-output-to-csv"
-df=spark.read.format('xml').option('rowType','books').load(data)
+data="C:\\bigdata\\DATASETS\\complexxmldata.xml"
+df=spark.read.format('xml').option('rowTag','catalog_item').load(data)
 ndf=flatten(df)
-print(ndf)
+ndf.show()
+print("Execution Success!!")
