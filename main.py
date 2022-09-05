38d201174b8d430224003362024a73b7949475e1
Chinmay Naik <94610060+ChinmayNaik27@users.noreply.github.com>
2022-09-05 17:03:50 +0530

    spark-practice

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and setting
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -14,3 +14,12 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#processing simple xml data
+from pyspark.sql import *
+from pyspark.sql.functions import *
+
+spark = SparkSession.builder.master("local[*]").appName("test").getOrCreate()
+sc = spark.sparkContext
+data="C:\\bigdata\\DATASETS\\xmldata.xml"
+df=spark.read.format('xml').option('rowType','course').load(data)
+df.show()
--- new1.py
+++ new1.py
@@ -1,18 +1,29 @@
 from pyspark.sql import *
 from pyspark.sql.functions import *
-import re
-spark = SparkSession.builder.master("local[*]").appName("test").getOrCreate()
-sc = spark.sparkContext
+from pyspark.sql.types import *
+spark=SparkSession.builder.master('local[*]').appName('testname').getOrCreate()
+sc=spark.sparkContext
+data="C:\\Users\\chinm\\Downloads\\bank-fullCopy.csv"
+df=sc.textFile(data)
+# res=df.first()
+res1=df.map(lambda x:x.replace("\"","")).filter(lambda x:x!=res)
+df1=spark.read.csv(res1,header=True,sep=';',inferSchema=True)
+df1.show()
+# res1=df.first()
+# res=df.map(lambda x:x.replace("\"",""))
+# res1=res.map(lambda x:x.split(';')).map(lambda x:(x[0],1)).reduceByKey(lambda x,y:x+y).sortBy(lambda x:x[0])
+# # print(res3.collect())
+# for i in res1.collect():
+#     print(i)
+# # res2=res.map(lambda x:x.split(',')).toDF(["CustomerID","CompanyName","ContactName","ContactTitle", "Address","City","Region","PostalCode","Country","Phone","Fax"])
+# # res2.show()
 
-data="C:\\bigdata\\DATASETS\\10000Records.csv"
+# newdf=df.select('id','name').subtract(df.select('id','name'))
+# newdf.show()
 
-df=spark.read.format('csv').option('header','true').option('inferSchema','true').load(data)
-# df.show()
-df.printSchema()
 
-cols=[re.sub(r'[^a-zA-Z0-9]',"",c.lower()) for c in df.columns]
-ndf=df.toDF(*cols)
-ndf2=ndf.withColumn("new3",to_date(col('dateofbirth'),'MM/dd/yyyy'))
-ndf2.show()
-ndf.printSchema()
-ndf2.show()
+from pyspark.sql import *
+
+spark=SparkSession.builder.master('local[2]').appName('test').getOrCreate()
+df=spark.read.format('org.apache.spark.sql.cassandra').option('keyspace','cassdb').option('table','asl').load()
+df.show()
