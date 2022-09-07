7adc24ce0f3e2e8e4e91d7b9f0579b39f2d4ff9b
Chinmay Naik <94610060+ChinmayNaik27@users.noreply.github.com>
2022-09-07 22:34:09 +0530

    spark-programs

---
+++ Sparkcore/pyspark104thproc.py
@@ -0,0 +1,18 @@
+#writing data to hbase after processing
+from pyspark.sql import *
+from pyspark.sql.functions import *
+
+spark=SparkSession.builder.master('local[2]').appName('test1').getOrCreate()
+
+df=spark.read.format('org.apache.phoenix.spark').option('table','EMP').option('zkUrl','localhost:2181').load()
+df.createOrReplaceTempView("tab")
+res=spark.sql("select * from tab where id=3")
+res.show()
+df.show()
+#the table should be present earlier to process it is compulsory for no sql db
+#to create tables on fly we have to write different code
+res.write.mode('overwrite').format("org.apache.phoenix.spark").option('table','newtab').option("zkUrl","localhost:2181").save()
+#using inline Query
+
+res1=spark.sql("select * from tab where id=(select max(id) from tab)")
+res1.show()
