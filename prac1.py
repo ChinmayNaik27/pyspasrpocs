090e48795a60286f3b12dfee680a7b1fd34f2cbf
Chinmay Naik <94610060+ChinmayNaik27@users.noreply.github.com>
2022-09-08 18:05:18 +0530

    Create prac1.py

---
+++ prac1.py
@@ -0,0 +1,8 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+
+spark = SparkSession.builder.master("local[*]").appName("test").getOrCreate()
+sc = spark.sparkContext
+
+
+df=spark.read.format('net.')
